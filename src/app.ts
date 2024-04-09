483d84b03c09dd52409a8c484b391f22fec060c4
Munir Abdullahi <abdullahimunir3@gmail.com>
2024-04-09 19:02:15 +0100

    refactor:added logger to replace console.log

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import CONFIG from '../config/default';
 import connect from './utils/connect';
+import log from './utils/logger'
 
 const app = express()
 
 app.listen(CONFIG.PORT, async() => {
-  console.log(`App is running at port:${CONFIG.PORT}`);
+  log.info(`App is running at http://localhost:${CONFIG.PORT}`);
   await connect()
   
 })
--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose'
 import CONFIG from '../../config/default'
+import log from './logger'
 
 async function connect() {
   try{
     await mongoose.connect(CONFIG.MONGODB_URI)
-    console.log('db successfully connected')
+    log.info('db successfully connected')
   }
   catch (error) {
-    console.log(`unable to connect to db ${error}`)
+    log.info(`unable to connect to db ${error}`)
     process.exit(1)
   }
 }
